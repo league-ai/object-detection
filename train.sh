af8ceba1d1c9dbc479c4290d769c502a861ca93e
Oliver Struckmeier <oliver.strckmeier@tutanota.de>
2023-04-04 16:48:39 +0300

    training on two separate models

--- train.sh
+++ train.sh
@@ -9,6 +9,6 @@
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user=oliver.struckmeier@aalto.fi
 source /scratch/work/strucko1/virtualenvs/leagueai/bin/activate
-#python train.py --workers 6 --device 0 --batch-size 20 --data data/leagueai.yaml --img 1280 1280 --cfg cfg/training/leagueai.yaml --weights 'yolov7-w6_training.pt' --name leagueai --hyp data/hyp.transfer.p5.leagueai.yaml --epochs 10
 #python train_aux.py --workers 8 --device 0 --batch-size 16 --data data/leagueai.yaml --img 1280 1280 --cfg cfg/training/leagueai.yaml --weights 'yolov7-w6_training.pt' --name leagueai --hyp data/hyp.transfer.p6.leagueai.yaml --epochs 150
-python train_aux.py --workers 8 --device 0 --batch-size 32 --data data/minimap.yaml --img 512 --cfg cfg/training/minimap.yaml --weights './runs/training/minimap8/weights/last.pkl' --name minimap --hyp data/hyp.minimap.yaml --epochs 200
+python train_aux.py --workers 8 --device 0 --batch-size 32 --data data/minimap.yaml --img 512 --cfg cfg/training/minimap.yaml --weights 'yolov7-w6_training.pt' --name minimap --hyp data/hyp.minimap.yaml --epochs 200
+#python train_aux.py --workers 8 --device 0 --batch-size 32 --data data/yuumi.yaml --img 512 --cfg cfg/training/yuumi.yaml --weights 'yolov7-w6_training.pt' --name yuumi --hyp data/hyp.yuumi.yaml --epochs 150
