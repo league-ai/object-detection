5132070e1b87a9641721f3e3bde86360b2409c38
Oliver Struckmeier <oliver.struckmeier@aalto.fi>
2023-04-06 14:04:48 +0300

    adding detection script

---
+++ detect.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#SBATCH --time=1:00:00
+#SBATCH --job-name=test
+#SBATCH --mem=8000
+#SBATCH --gres=gpu:1
+#SBATCH --output=test.out
+#SBATCH --cpus-per-task=9
+#SBATCH --constraint='volta'
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user=oliver.struckmeier@aalto.fi
+source /scratch/work/strucko1/virtualenvs/leagueai/bin/activate
+#python detect.py --weights ./runs/train/yuumi2/weights/best.pt --conf 0.85 --img-size 512 --source ./6179898202.avi --no-trace
+python detect.py --weights ./runs/train/minimap2/weights/best.pt --conf 0.5 --img-size 512 --source ./6179898202.avi --no-trace
